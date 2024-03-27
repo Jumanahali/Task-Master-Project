19b7655fabe3b124954c3e3545e2eb9b99b5399a
RawanSaud23 <srawanalsaif@gmail.com>
2024-03-27 19:33:30 +0300

    alt for time and loc update

--- app/src/main/java/com/example/taskmaster/ActiveOrder.java
+++ app/src/main/java/com/example/taskmaster/ActiveOrder.java
@@ -5,6 +5,8 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextClock;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,6 +31,7 @@ public class ActiveOrder extends AppCompatActivity {
             return insets;
         });
 
+        //alt cancel
         btnCancle= findViewById(R.id.CancleOreder);
         builder= new AlertDialog.Builder(this);
 
@@ -37,6 +40,58 @@ public class ActiveOrder extends AppCompatActivity {
             public void onClick(View v) {
                 builder.setTitle("CANCELLTION COFIRMATION!")
                         .setMessage("Are you sure you want to cancel your order?")
+                        .setCancelable(true)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                finish(); //next phase we will make it work as it should be :)
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                dialog.dismiss(); // Close the dialog
+                            }
+                        })
+                        .show();
+            }
+        });
+
+        //Alt time
+        TextClock TxtTime = findViewById(R.id.textClock);
+        builder= new AlertDialog.Builder(this);
+
+        btnCancle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                builder.setTitle("UPDATE TIME COFIRMATION!")
+                        .setMessage("Are you sure you want to update time of your order?")
+                        .setCancelable(true)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                finish(); //next phase we will make it work as it should be :)
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                dialog.dismiss(); // Close the dialog
+                            }
+                        })
+                        .show();
+            }
+        });
+
+        //alt loc
+        TextView TxtLoc = findViewById(R.id.loc);
+        builder= new AlertDialog.Builder(this);
+
+        btnCancle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                builder.setTitle("UPDATE LOCATION COFIRMATION!")
+                        .setMessage("Are you sure you want to update your location for the order?")
                         .setCancelable(true)
                         .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                             @Override
